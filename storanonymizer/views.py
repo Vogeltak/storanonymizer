fe1b072a35d85da7763144239912c2928d3e7dd5
Vogeltak <vogeltak@gmail.com>
2016-06-24 19:05:11 +0200

    Change redirect after logout

--- storanonymizer/views.py
+++ storanonymizer/views.py
@@ -26,7 +26,7 @@ def login():
 @login_required
 def logout():
 	logout_user()
-	return redirect(url_for("login"))
+	return redirect(url_for("index"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
